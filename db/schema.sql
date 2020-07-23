5e164a80ec998fce80cc538a8deb03574258945b
Mai Banh <65269951+mtbanh@users.noreply.github.com>
2020-07-22 23:30:27 -0700

    created db and mysql connection to node

---
+++ db/schema.sql
@@ -0,0 +1,11 @@
+DROP DATABASE IF EXISTS vietnamese_foods_db;
+
+CREATE DATABASE vietnamese_foods_db;
+
+USE vietnamese_foods_db;
+
+CREATE TABLE foods(
+id INT(30) NOT NULL auto_increment PRIMARY KEY,
+food_name VARCHAR(300) NOT NULL,
+devoured boolean NOT NULL
+);
---
+++ db/seeds.sql
@@ -0,0 +1,2 @@
+INSERT INTO foods (food_name, devoured) 
+VALUES ('Banh Mi', true), ('Pho', false), ('Bun Bo Hue', true);
