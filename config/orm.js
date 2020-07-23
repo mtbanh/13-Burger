1778b37dda285e238770e359e674dca583755631
Mai Banh <65269951+mtbanh@users.noreply.github.com>
2020-07-23 16:53:42 -0700

    started orm file

--- config/orm.js
+++ config/orm.js
@@ -1,15 +1,44 @@
-const connection = require('./connection.js');
+var connection = require('./connection.js');
 
-const selectAll =()=>{
+var printQuestionMarks = (num) =>{
+    var arr =[];
+    
+    for(i=0; i<num; i++){
+        arr.push('?');
+    }
 
-};
+    return arr.toString();
+}
 
-const insertOne =()=>{
+var objToSql = (ob) =>{
+    var arr = [];
+    for(var key in ob){
+        var value = ob[key];
 
-};
+        if(Object.hasOwnProperty.call(ob, key)) {
+            if(typeOf value === 'string' && value.indexOf(" ") >= 0){
+                value = "'" + value + "'";
+            }
 
-const updateOne =()=>{
+            arr.push(key+ '=' + value);
+        }
+    }
 
-};
+    return arr.toString();
+}
+var orm = {
+    selectAll: (tableInput, cb)=>{
+        var queryStr = 'SELECT * FROM ' + tableInput + ';';
+        connection.query(queryStr, (err, result)=>{
+            if(err) return err;
+            cb(result);
+        })
+    },
 
+    insertOne: (table, col, val, cb)=>{
+        //INSERT INTO table_name (col) VALUES (val)
+        var queryStr = 'INSERT INTO ' + table;
+        queryStr += ` (${col}) VALUES = ??`
+        }
+}
 module.exports = orm;
